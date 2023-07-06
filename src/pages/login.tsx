129265a221dd7988909bef24a15cb9ba3e8600b4
Gabriel Pedroza <gpaebdez@meta.com>
2023-07-06 09:36:25 -0700

    basic page routing

---
+++ src/pages/login.tsx
@@ -0,0 +1,36 @@
+import { getSession, signIn } from 'next-auth/react'
+import { GetServerSideProps } from 'next';
+import Link from 'next/link';
+
+const Login = () => {
+
+  return (
+    <>
+      <div>Login</div>
+      <Link href={'/'}>go to landing page</Link>
+      <br />
+      <button onClick={() => void signIn('github')}>sign in with github</button>
+      <br />
+      <button onClick={() => void signIn('google')}>sign in with google</button>
+      <br />
+    </>
+  )
+}
+
+export default Login
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/home",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
