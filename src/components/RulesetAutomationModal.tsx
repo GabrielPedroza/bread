5158e28d8c9ba1bfbe07813d706b2492ae4e250a
Gabriel Pedroza <gpaebdez@meta.com>
2023-07-12 10:58:22 -0700

    Ruleset automation modal with FormType component

--- src/components/RulesetAutomationModal.tsx
+++ src/components/RulesetAutomationModal.tsx
@@ -1,29 +1,37 @@
-import { useContext } from "react"
-import { ModalContext } from "~/state/ModalContext"
+import { useContext } from "react";
+import { ModalContext } from "~/state/ModalContext";
+import FormType from "./FormType";
 
 const RulesetAutomationModal = () => {
+  const { setCreateModalOpened, setRulesetModalOpened, setEventTrigger } = useContext(ModalContext);
 
-  const { setCreateModalOpened, setRulesetModalOpened } = useContext(ModalContext)
+  const handleClose = () => {
+    setCreateModalOpened(false)
+    setRulesetModalOpened(false)
+    setEventTrigger("") // not neccessary but nice to know state is always up-to-date
+  }
 
   return (
     <div className="fixed inset-0 flex items-center justify-center">
-      <div className="bg-orange-500 absolute w-[500px] h-[800px] z-100">
-        <p>
-          Ruleset Modal
-        </p>
-        <button className="absolute right-4 top-4 text-3xl p-4" onClick={() => {
-          setRulesetModalOpened(false)
-          setCreateModalOpened(false)
-        }}>X</button>
-        <p>if else done here</p>
-        <button className="absolute bottom-4 right-4" onClick={() => {
-          setCreateModalOpened(false)
-          setRulesetModalOpened(false)
-        }}>Create Automation</button>
-        <button className="absolute bottom-4 left-4" onClick={() => setRulesetModalOpened(false)}>go back</button>
+      <div className="relative bg-orange-400 rounded-lg shadow-lg p-6 w-[500px] h-[800px]">
+        <h3 className="text-xl font-bold mb-4">Ruleset Modal</h3>
+        <FormType />
+        <div className="flex justify-between mt-4">
+        <button
+          className="bg-gray-300 text-gray-700 px-4 py-2 rounded"
+          type="button"
+          aria-label="Go Back to Create Automation Modal"
+          onClick={() => setRulesetModalOpened(false)}
+        >
+          Go Back
+        </button>
+        </div>
+        <button type="button" className="absolute text-black top-4 right-4 text-4xl" aria-label="Close Both Create and Ruleset Modals" onClick={handleClose}>
+          X
+        </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default RulesetAutomationModal
+export default RulesetAutomationModal;
