a70933ef1edc16c6fe9d9bcfc785b4742489f564
Gabriel Pedroza <gpaebdez@meta.com>
2023-07-12 10:49:43 -0700

    Create modal logic | saves eventTrigger so Ruleset modal is aware of what form to show

--- src/components/CreateAutomationModal.tsx
+++ src/components/CreateAutomationModal.tsx
@@ -1,9 +1,31 @@
-import { useContext } from "react"
-import { ModalContext } from "~/state/ModalContext"
+import { MouseEvent, useContext } from "react"
+import { z } from "zod";
+import { ModalContext, ModalContextType } from "~/state/ModalContext"
+
+const eventTypeSchema = z.array(z.object({
+  id: z.number(),
+  name: z.enum(['Calendar', 'Mail', 'GitHub', '']),
+  description: z.string()
+}))
+
+type eventTypes = z.infer<typeof eventTypeSchema>
 
 const CreateAutomationModal = () => {
-
-  const { setCreateModalOpened, setRulesetModalOpened } = useContext(ModalContext)
+  
+  const { setCreateModalOpened, setRulesetModalOpened, setEventTrigger } = useContext(ModalContext)
+  
+  const eventTypes: eventTypes = [
+    { id: 1, name: 'Mail', description: 'Example: When an email was receieved...' },
+    { id: 2, name: 'GitHub', description: 'Example: When a PR has been merged to a repository...' },
+    { id: 3, name: 'Calendar', description: 'Example: When a meeting is canceled...' },
+    // event types
+  ];
+  
+  const handleClickedAutomation = (e: MouseEvent<HTMLDivElement>, eventType: eventTypes[0]["name"]) => {
+    const eventTrigger = eventType.toLowerCase() as ModalContextType["eventTrigger"]
+    setEventTrigger(eventTrigger)
+    setRulesetModalOpened(true)
+  }
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-opacity-[15%] bg-slate-400">
@@ -11,9 +33,15 @@ const CreateAutomationModal = () => {
         <p>
           Create Modal
         </p>
-        <button className="absolute right-4 top-4 text-3xl p-4" onClick={() => setCreateModalOpened(false)}>X</button>
-        <p>Different types of events user can do (mail, calendar, github)</p>
-        <button className="absolute bottom-4 right-4" onClick={() => setRulesetModalOpened(true)}>continue to ruleset modal</button>
+        <button className="absolute right-4 top-4 text-3xl p-4" aria-label="Close Create Modal" type="button" onClick={() => setCreateModalOpened(false)}>X</button>
+        <div className="flex flex-col gap-4 p-4 mt-10">
+          {eventTypes.map((eventType) => (
+            <div key={eventType.id} className="border-2 p-4 cursor-pointer" onClick={(e) => handleClickedAutomation(e, eventType.name)}>
+              <h3 className="text-xl font-bold">{eventType.name}</h3>
+              <p>{eventType.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   )
