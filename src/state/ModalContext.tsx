b12c28c3041654f44a59b6da34deb3809406b04c
Gabriel Pedroza <gpaebdez@meta.com>
2023-07-12 10:44:46 -0700

    eventTrigger in state management (useContext) for type of ruleset form

--- src/state/ModalContext.tsx
+++ src/state/ModalContext.tsx
@@ -2,19 +2,23 @@ import { createContext, useState } from "react";
 import { z } from "zod";
 
 const ModalContextSchema = z.object({
+  eventTrigger: z.enum(["mail", "calendar", "github", ""]),
   createModalOpened: z.boolean(),
   rulesetModalOpened: z.boolean(),
   setCreateModalOpened: z.function(z.tuple([z.boolean()])).returns(z.void()),
-  setRulesetModalOpened: z.function(z.tuple([z.boolean()])).returns(z.void())
+  setRulesetModalOpened: z.function(z.tuple([z.boolean()])).returns(z.void()),
+  setEventTrigger: z.function(z.tuple([z.enum(["mail", "calendar", "github", ""])])).returns(z.void())
 });
 
-type ModalContextType = z.infer<typeof ModalContextSchema>;
+export type ModalContextType = z.infer<typeof ModalContextSchema>;
 
 const InitialModalContext: ModalContextType = {
+  eventTrigger: "",
   createModalOpened: false,
   rulesetModalOpened: false,
   setCreateModalOpened: () => {},
-  setRulesetModalOpened: () => {}
+  setRulesetModalOpened: () => {},
+  setEventTrigger: () => {}
 };
 
 export const ModalContext = createContext<ModalContextType>(InitialModalContext);
@@ -25,13 +29,16 @@ type ModalProviderProps = {
 
 export const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
   const [createModalOpened, setCreateModalOpened] = useState<boolean>(false);
+  const [eventTrigger, setEventTrigger] = useState<ModalContextType["eventTrigger"]>("");
   const [rulesetModalOpened, setRulesetModalOpened] = useState<boolean>(false);
 
   const contextValues: ModalContextType = {
+    eventTrigger,
     createModalOpened,
     rulesetModalOpened,
     setCreateModalOpened: (value: boolean) => setCreateModalOpened(value),
-    setRulesetModalOpened: (value: boolean) => setRulesetModalOpened(value)
+    setRulesetModalOpened: (value: boolean) => setRulesetModalOpened(value),
+    setEventTrigger: (value: ModalContextType["eventTrigger"]) => setEventTrigger(value)
   };
 
   return (
