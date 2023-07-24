196d7eada85c22ec4bf3b1f3d6f95db456f00425
Gabriel Pedroza <gpaebdez@meta.com>
2023-07-23 20:01:37 -0700

    locate webhooks by hookID

--- src/pages/api/webhooks/webhook.ts
+++ src/pages/api/webhooks/webhook.ts
@@ -2,16 +2,6 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import * as crypto from "crypto";
 import { env } from '~/env.mjs';
 
-interface IssuePayload {
-  issue: {
-    html_url: string;
-    title: string;
-  };
-  repository: {
-    name: string;
-  };
-}
-
 export default function webhooksHandler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -28,17 +18,9 @@ export default function webhooksHandler(
     res.status(401).send("Unauthorized");
     return;
   }
-
+  
   if (req.method === 'POST') {
-    const payload = req.body as IssuePayload;
-    const issueTitle = payload.issue.title;
-    const repositoryName = payload.repository.name;
-    const issueUrl = payload.issue.html_url;
-
-    console.log(`Webhook received for issue: ${issueTitle}`);
-    console.log(`Repository: ${repositoryName}`);
-    console.log(`Issue URL: ${issueUrl}`);
-
+    console.log('webhook received: ', req.headers["x-github-hook-id"]);
     res.status(200).send('Webhook received successfully');
   }
 }
