dd42a7daf33895a5cf1baee53b7052a8376f6206
Gabriel Pedroza <gpaebdez@meta.com>
2023-07-25 17:57:17 -0700

    commenting more on ambiguity | giving gh error a type

--- src/server/api/routers/webhook.ts
+++ src/server/api/routers/webhook.ts
@@ -3,8 +3,13 @@ import { z } from "zod";
 import { env } from "~/env.mjs";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
+interface GitHubAPIError {
+  status: number;
+}
+
 export const webhookRouter = createTRPCRouter({
-  // accessToken is optional because there can be instances where the user's session is remembered which makes the accessToken undefined.
+  // accessToken is optional because there can be instances where the user's session is remembered (automatically is redirected to hompage) 
+  // which makes the accessToken undefined since accessToken is grabbed when user is loggin in (authenticating).
   // if this occurs, we use the accessToken in the DB and if that is stale, we require the user to log out and log back in.
   createWebhook: protectedProcedure.input(z.object({ accessToken: z.string().optional() })).mutation(async ({ ctx, input }) => {
     const owner = "GabrielPedroza";
@@ -26,8 +31,6 @@ export const webhookRouter = createTRPCRouter({
   
   // if access token on DB is stale, update it. Not doing so will cause a 401 Error: Bad Credentials
   if (input.accessToken != undefined && input.accessToken !== accessToken) {
-    console.log("update token");
-    
     await ctx.prisma.account.update({
       where: {
         userId: ctx.session.user.id,
@@ -37,7 +40,7 @@ export const webhookRouter = createTRPCRouter({
       },
     });
   }
-
+    // getting authorization using accessToken to create webhook
     const octokit = new Octokit({
       auth: input.accessToken || accessToken
     });
@@ -72,14 +75,25 @@ export const webhookRouter = createTRPCRouter({
         const lastURLSlash = locationURL.lastIndexOf("/");
         hookID = locationURL.slice(lastURLSlash + 1);
         console.log("Webhook created successfully", hookID);
-      } else {
-        return false;
       }
 
     } catch (error) {
+      if (isGitHubAPIError(error)) {
+        // if 422: hook already exists in the repository
+        if (error.status === 422) {
+          return 422
+        }
+      }
+      // a possible error can be 401: Bad Credentials if DB accessToken is stale AND user's session is remembered. 
+      // We require the user to log out and log in.
       return false;
     }
     // returning hookID so automation model can grab it. occurs in src/components/FormType.tsx
     return hookID;
   }),
 });
+  {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+const isGitHubAPIError = (error: any): error is GitHubAPIError => {
+  {/* eslint-disable-next-line @typescript-eslint/no-unsafe-member-access */}
+  return typeof error.status === "number"
+}
