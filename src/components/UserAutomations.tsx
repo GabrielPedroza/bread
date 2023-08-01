29f2f23222aa67534daf8a827bc1aec2889e400c
Gabriel Pedroza <gpaebdez@meta.com>
2023-08-01 11:02:27 -0700

    UserAutomation void type eslint ignored

--- src/components/UserAutomations.tsx
+++ src/components/UserAutomations.tsx
@@ -1,7 +1,7 @@
 import { useSession } from "next-auth/react";
 import { type AutomationType } from "~/server/types/automation";
 import { api } from "~/utils/api";
-import { AiFillDelete, AiOutlineLoading } from "react-icons/ai"
+import { AiFillDelete, AiOutlineLoading } from "react-icons/ai";
 import { toast } from "react-hot-toast";
 import { useState } from "react";
 
@@ -9,58 +9,66 @@ const UserAutomations = () => {
   const userAutomations = api.automation.getUserAutomations.useQuery();
   const deleteAutomation = api.automation.deleteAutomation.useMutation();
   const deleteWebHook = api.webhook.deleteWebhook.useMutation();
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const { data: session } = useSession();
 
-  const handleDelete = async (automation: AutomationType, e: React.FormEvent) => {
-    e.preventDefault()
-    setLoading(true)
+  const handleDelete = async (
+    automation: AutomationType,
+    e: React.FormEvent
+  ) => {
+    e.preventDefault();
+    setLoading(true);
     if (loading) {
-      toast.dismiss()
-      toast.error("Stop spamming 😡")
+      toast.dismiss();
+      toast.error("Stop spamming 😡");
     } else {
-      toast.dismiss()
-      toast.loading("Deleting Automation")
+      toast.dismiss();
+      toast.loading("Deleting Automation");
       const deleteWebHookAndReturnHookID = await deleteWebHook.mutateAsync({
         hookID: Number(automation.webhookID),
         accessToken: session?.user.accessToken,
       });
-      
-      if (deleteWebHookAndReturnHookID && deleteWebHookAndReturnHookID !== 404) {
+
+      if (
+        deleteWebHookAndReturnHookID &&
+        deleteWebHookAndReturnHookID !== 404
+      ) {
         await deleteAutomation.mutateAsync({ hookID: automation.webhookID });
       } else if (deleteWebHookAndReturnHookID === 404) {
-        toast.dismiss()
-        toast.error("Cannot find your WebHook")
-        setLoading(false)
-        return
+        toast.dismiss();
+        toast.error("Cannot find your WebHook");
+        setLoading(false);
+        return;
       } else {
-        toast.dismiss()
-        toast.error("Log Out and Log Back In")
-        setLoading(false)
-        return
+        toast.dismiss();
+        toast.error("Log Out and Log Back In");
+        setLoading(false);
+        return;
       }
-      toast.dismiss()
-      toast.success('Automation Deleted', {
+      toast.dismiss();
+      toast.success("Automation Deleted", {
         style: {
-          border: '1px solid #713200',
-          padding: '16px',
-          color: '#713200',
+          border: "1px solid #713200",
+          padding: "16px",
+          color: "#713200",
         },
         iconTheme: {
-          primary: '#713200',
-          secondary: '#FFFAEE',
+          primary: "#713200",
+          secondary: "#FFFAEE",
         },
       });
-      await userAutomations.refetch()
-      setLoading(false)
+      await userAutomations.refetch();
+      setLoading(false);
     }
   };
 
   return (
-    <div className="mx-auto mt-5 h-[45%] w-11/12 relative">
-      <h2 className="relative top-4 left-8 text-3xl mb-10 text-amber-900 font-bold">My Automations</h2>
-      <div className="flex justify-around mb-6 mt-5 text-xl text-amber-950">
+    <div className="relative mx-auto mt-5 h-[45%] w-11/12">
+      <h2 className="relative left-8 top-4 mb-10 text-3xl font-bold text-amber-900">
+        My Automations
+      </h2>
+      <div className="mb-6 mt-5 flex justify-around text-xl text-amber-950">
         <h3>Name</h3>
         <h3>Description</h3>
         <h3>Status</h3>
@@ -69,25 +77,47 @@ const UserAutomations = () => {
         if (userAutomations.data) {
           if (userAutomations.data.length > 0) {
             return userAutomations.data.map((automation, i) => (
-              <div className="relative flex justify-around w-[95%] border-2 border-amber-950 rounded-lg mx-auto py-3 mb-4 transition-all hover:scale-[101%]" key={i}>
-                <div className="w-40 truncate -ml-10 top-[0.15rem] relative text-amber-950">{automation.name}</div>
-                  <div className="w-44 truncate -ml-10 top-[0.15rem] relative text-amber-950 hover:">{automation.desc}</div>
-                  <div>{automation.status === "active" ? <p className="text-lg relative top-[0.05rem]">🟢</p> : <p>🔴</p>}</div>
-                  <button
-                  className="absolute right-4 top-[0.65rem] text-lg p-2"
-                    onClick={(e) => {
-                      handleDelete(automation, e)
-                    }}
-                  >
-                    <AiFillDelete />
-                  </button>
+              <div
+                className="relative mx-auto mb-4 flex w-[95%] justify-around rounded-lg border-2 border-amber-950 py-3 transition-all hover:scale-[101%]"
+                key={i}
+              >
+                <div className="relative top-[0.15rem] -ml-10 w-40 truncate text-amber-950">
+                  {automation.name}
+                </div>
+                <div className="hover: relative top-[0.15rem] -ml-10 w-44 truncate text-amber-950">
+                  {automation.desc}
+                </div>
+                <div>
+                  {automation.status === "active" ? (
+                    <p className="relative top-[0.05rem] text-lg">🟢</p>
+                  ) : (
+                    <p>🔴</p>
+                  )}
+                </div>
+                <button
+                  className="absolute right-4 top-[0.65rem] p-2 text-lg"
+                  onClick={(e) => {
+                    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+                    handleDelete(automation, e);
+                  }}
+                >
+                  <AiFillDelete />
+                </button>
               </div>
             ));
           } else {
-            return <div className="text-4xl text-center text-amber-950 relative top-24">Waiting for your first creation!</div>;
+            return (
+              <div className="relative top-24 text-center text-4xl text-amber-950">
+                Waiting for your first creation!
+              </div>
+            );
           }
         } else {
-          return <div className="animate-spin text-6xl absolute bottom-36 right-[37rem]"><AiOutlineLoading /></div>;
+          return (
+            <div className="absolute bottom-36 right-[37rem] animate-spin text-6xl">
+              <AiOutlineLoading />
+            </div>
+          );
         }
       })()}
     </div>
