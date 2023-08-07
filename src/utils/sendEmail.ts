997feee2a0bcd8a5524744ed5771af7fe4b11026
Gabriel Pedroza <gpaebdez@meta.com>
2023-08-06 19:47:16 -0700

    Email Formatter

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -17,10 +17,45 @@ export type AutomationAndActionByWebHookIDType = {
 export const sendEmail = (
   automationAndActionByWebHookID: AutomationAndActionByWebHookIDType,
   bodyResponse: unknown,
-  eventType: string
+  eventType: "issues" | "pull_request" | "star" | "push"
 ) => {
   const scheduleSendInSeconds =
     automationAndActionByWebHookID.action.scheduleSend;
+
+    let html = "";
+
+    // Format the email content based on the event type
+    if (eventType === "issues") {
+      html = `
+        <h1>Issue Event</h1>
+        <p>Issue created or updated!</p>
+        <pre>${JSON.stringify(bodyResponse, null, 2)}</pre>
+      `;
+    } else if (eventType === "pull_request") {
+      html = `
+        <h1>Pull Request Event</h1>
+        <p>Pull request opened or updated!</p>
+        <pre>${JSON.stringify(bodyResponse, null, 2)}</pre>
+      `;
+    } else if (eventType === "star") {
+      html = `
+        <h1>Star Event</h1>
+        <p>Repository starred or removed</p>
+        <pre>${JSON.stringify(bodyResponse, null, 2)}</pre>
+      `;
+    } else if (eventType === "push") {
+      html = `
+        <h1>Push Event</h1>
+        <p>Code pushed to repository!</p>
+        <pre>${JSON.stringify(bodyResponse, null, 2)}</pre>
+      `;
+    } else {
+      html = `
+        <h1>Unknown Event Type</h1>
+        <p>Received an unknown event type</p>
+        <pre>${JSON.stringify(bodyResponse, null, 2)}</pre>
+      `;
+    }
 
   sgMail.setApiKey(env.SEND_GRID_API_KEY);
 
@@ -28,7 +63,7 @@ export const sendEmail = (
     to: automationAndActionByWebHookID.action.toEmail, // enter your email here for testing purposes
     from: "BreadForMeta@gmail.com",
     subject: automationAndActionByWebHookID.action.subject,
-    text: JSON.stringify(bodyResponse),
+    html,
     sendAt: Math.floor(Date.now() / 1000 + scheduleSendInSeconds * 60), // defer email in UNIX Timestamp
   };
   // eslint-disable-next-line @typescript-eslint/no-floating-promises
